50ed173b145d40c07e829472ac6e1fa3fd54ff59
Ankur Goel <ankurgel@gmail.com>
2014-03-24 23:10:57 +0530

    First commit. Added gems and bundled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paste::Application.config.secret_key_base = 'b1c8b3f94f8a0f0d5a58bd7d0907595dc8f199fe941d1aee76762a1b5f5141984f558fc2af23b96d2c9e611203d6e6c6c943635902d98724597eaf33b693787d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Paste::Application.config.session_store :cookie_store, key: '_paste_session'
