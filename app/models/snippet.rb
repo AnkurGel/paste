b53c66195e76cc5ec98d82105cab788dd2175e79
Ankur Goel <ankurgel@gmail.com>
2014-03-26 17:02:30 +0530

    Pygmentation support added which will be handled by delayed_job as background process

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,2 +1,10 @@
+require 'net/http'
 class Snippet < ActiveRecord::Base
+    
+
+  def pygmentation
+    uri = URI.parse('http://pygments.appspot.com/')
+    request = Net::HTTP.post_form(uri, { lang: language, code: raw })
+    update_attribute(:highlighted, request.body)
+  end
 end
