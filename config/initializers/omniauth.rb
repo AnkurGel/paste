67c2b14e5747885dbfd3e89d4373413fd234c827
Ankur Goel <ankurgel@gmail.com>
2014-03-28 17:30:03 +0530

    abstracted key & secret in env-variables for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
-credentials = YAML.load_file("#{Rails.root}/secret_credentials.yaml")
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, credentials['key'], credentials['secret'], scope: "user,repo,gist"
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,repo,gist"
 end
